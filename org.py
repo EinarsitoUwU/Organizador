58b5c26a68ec2ab7955d59bb7aa6ff26d3d9804d
Einar Cabrera <einarjahazielcabreraamador@hotmail.com>
2023-05-02 16:06:24 -0600

    Version 1

---
+++ org.py
@@ -0,0 +1,57 @@
+import os
+import glob
+import shutil
+lst = []
+ruta = "/Users/Einar/Downloads/"
+files = glob.glob("" + ruta + "*.*")
+print(files)
+
+
+def splitear(cadena):
+    u = cadena.split(".")
+    a = u[1]
+    if a == None:
+        return cadena
+    else:
+        return a
+def extensions():
+    for x in files:
+        extension = splitear(x)
+        print(extension)
+        return extension
+def splitear2(cadena):
+    u = cadena.split("\\")
+    a = u[1]
+    if a == None:
+        return cadena
+    else:
+        return a
+def dirs():
+    for x in files:
+            global z
+            u = splitear(x)
+            z = u.upper()
+            print(z)
+            
+            test = os.path.exists(ruta+z)
+            if  test == True or z == "INI":
+                #print("ya existe")
+                if z == "INI":
+                    print()
+                else:
+                    shutil.move(ruta+splitear2(x),ruta+z+"/"+splitear2(x))
+            else:
+                
+                os.makedirs("" + ruta + "" +z+"", exist_ok=True)
+                lst.append(z)
+                for y in files:
+                    if splitear(splitear2(y)).upper() == z:
+                        shutil.move(""+ruta+""+splitear2(y)+"",""+ruta+""+z+"/"+splitear2(y)+"")
+                        #print("que onda")
+                    else:
+                        print()
+                    
+#Execute
+
+dirs()
+
